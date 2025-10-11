c7e64662b469dd928f5131c7c295e9f8c26ceb8c
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-10-11 10:57:47 +0530

    Update main.c

--- Assignment 5/main.c
+++ Assignment 5/main.c
@@ -1,61 +1,73 @@
 #include <stdio.h>
-#include <limits.h>
 
-// Function to print the optimal parenthesis placement
-void printParenthesis(int i, int j, int n, int bracket[n][n], char *name) {
-    if (i == j) {
-        printf("%c", (*name)++);
-        return;
-    }
-    printf("(");
-    printParenthesis(i, bracket[i][j], n, bracket, name);
-    printParenthesis(bracket[i][j] + 1, j, n, bracket, name);
-    printf(")");
-}
+// Function to sort activities according to finish time (using simple bubble sort)
+void sortActivities(int start[], int finish[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (finish[j] > finish[j + 1]) {
+                // Swap finish times
+                temp = finish[j];
+                finish[j] = finish[j + 1];
+                finish[j + 1] = temp;
 
-// Matrix Chain Multiplication using Dynamic Programming
-void matrixChainOrder(int p[], int n) {
-    int m[n][n];       // m[i][j] = minimum number of multiplications needed
-    int bracket[n][n]; // To store the position of split
-
-    // Cost is 0 when multiplying one matrix
-    for (int i = 1; i < n; i++)
-        m[i][i] = 0;
-
-    // L is the chain length
-    for (int L = 2; L < n; L++) {
-        for (int i = 1; i < n - L + 1; i++) {
-            int j = i + L - 1;
-            m[i][j] = INT_MAX;
-            for (int k = i; k < j; k++) {
-                int cost = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-                if (cost < m[i][j]) {
-                    m[i][j] = cost;
-                    bracket[i][j] = k;
-                }
+                // Swap corresponding start times
+                temp = start[j];
+                start[j] = start[j + 1];
+                start[j + 1] = temp;
             }
         }
     }
+}
 
-    printf("Minimum number of multiplications is: %d\n", m[1][n - 1]);
-    printf("Optimal Parenthesization is: ");
-    char name = 'A';
-    printParenthesis(1, n - 1, n, bracket, &name);
-    printf("\n");
+// Function to perform activity selection
+void activitySelection(int start[], int finish[], int n) {
+    int i, j;
+
+    printf("Following activities are selected:\n");
+
+    // The first activity always gets selected
+    i = 0;
+    printf("Activity %d (Start: %d, Finish: %d)\n", i + 1, start[i], finish[i]);
+
+    // Consider rest of the activities
+    for (j = 1; j < n; j++) {
+        // If this activity starts after or when the last selected one finishes
+        if (start[j] >= finish[i]) {
+            printf("Activity %d (Start: %d, Finish: %d)\n", j + 1, start[j], finish[j]);
+            i = j;  // Update i to the last selected activity
+        }
+    }
 }
 
 int main() {
-    // Example: 4 matrices with dimensions
-    // A1 = 10x20, A2 = 20x30, A3 = 30x40, A4 = 40x30
-    int arr[] = {10, 20, 30, 40, 30};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n, i;
 
-    printf("Matrix dimensions: ");
-    for (int i = 0; i < n - 1; i++)
-        printf("A%d(%dx%d) ", i + 1, arr[i], arr[i + 1]);
-    printf("\n\n");
+    printf("Enter number of activities: ");
+    scanf("%d", &n);
 
-    matrixChainOrder(arr, n);
+    int start[n], finish[n];
+
+    printf("Enter start times of activities:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &start[i]);
+    }
+
+    printf("Enter finish times of activities:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &finish[i]);
+    }
+
+    // Sort activities by finish time
+    sortActivities(start, finish, n);
+
+    printf("\nActivities sorted by finish time:\n");
+    for (i = 0; i < n; i++) {
+        printf("Activity %d: Start = %d, Finish = %d\n", i + 1, start[i], finish[i]);
+    }
+
+    printf("\nActivity Selection Problem using Greedy Approach\n\n");
+    activitySelection(start, finish, n);
 
     return 0;
 }
